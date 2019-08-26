5b81d3a256e5a0daa6625a82e1971d9d9fd43558
Xie_Junkang <42465253+1583902733@users.noreply.github.com>
2019-08-26 21:51:02 +0800

    Create binarySearch.cpp

---
+++ chapter 1/binarySearch.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//a数组为递增数组
+int binarySearch(vector<int> a, int x)
+{
+    int low = 0, high = a.size();
+    int j = 0;
+    while (low <= high && j == 0)
+    {
+        int mid = (low + high) / 2;
+        if (a[mid] == x)
+            j = mid;
+        else if (a[mid] < x)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return j;
+}
+
+int main()
+{
+    vector<int> A = {1,3,4,5,7,9};
+    cout<<binarySearch(A,4)<<endl;
+    return 0;
+}
